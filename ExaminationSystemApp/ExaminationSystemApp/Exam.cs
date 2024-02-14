7c4016eed3eda46a6d78b1e9520c285a4374a9ac
Ali-nasser1 <50531711+Ali-nasser1@users.noreply.github.com>
2024-02-14 19:33:43 +0200

    Add files via upload

---
+++ ExaminationSystemApp/ExaminationSystemApp/Exam.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaminationSystemApp
+{
+    internal abstract class Exam
+    {
+       protected int time { get; set; }
+       protected int numberOfQuestions { get; set; }
+       public Question[] examQuestion;
+       public abstract void ShowExam();
+       public abstract void CreateExam();
+    }
+}
---
+++ ExaminationSystemApp/ExaminationSystemApp/Subject.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaminationSystemApp
+{
+    internal class Subject
+    {
+        private int id { get; set; }
+        private string name { get; set; }
+        public int Type;
+        Exam exam;
+        public Subject(int _id, string _name) 
+        {
+            id = _id;
+            name = _name;
+        }
+        public void CreateExam()
+        {
+            do
+            {
+                Console.WriteLine("What's the type of your exam 1 for final or 2 for practical ?");
+            } while(!int.TryParse(Console.ReadLine(), out Type) && (Type == 1 || Type == 2));
+
+            if(Type == 1)
+            {
+                exam = new FinalExam(); // dynamic bindding
+                exam.CreateExam();
+
+                Console.WriteLine("Do you want to begin the exam ? press (Y | N)");
+                char begin = char.Parse(Console.ReadLine());
+                while ((begin != 'Y' && begin != 'y') && (begin != 'N' && begin != 'n'))
+                {
+                    Console.WriteLine("please enter a valid choice !");
+                    begin = char.Parse(Console.ReadLine());
+                }
+                if(begin == 'Y' ||  begin == 'y')
+                 exam.ShowExam();
+            }
+            else if(Type == 2) 
+            {
+                exam = new PracticalExam(); // dynamic bindding
+                exam.CreateExam();
+                Console.WriteLine("Do you want to begin the exam ? press (Y | N)");
+                char begin = char.Parse(Console.ReadLine());
+                while ((begin != 'Y' && begin != 'y') && (begin != 'N' && begin != 'n'))
+                {
+                    Console.WriteLine("please enter a valid choice !");
+                    begin = char.Parse(Console.ReadLine());
+                }
+                if (begin == 'Y' || begin == 'y')
+                    exam.ShowExam();
+            }
+        }
+    }
+}
